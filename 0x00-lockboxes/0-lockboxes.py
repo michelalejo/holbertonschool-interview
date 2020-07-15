57ff5359d70bf62584a4d1d8cd6906767552f3e1
michelalejo <michelmolina65@gmail.com>
2020-07-15 13:09:17 -0500

    0-lockboxes.py

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    keys = [0]
+    for i in keys:
+        for j in boxes[i]:
+            if j not in keys:
+                keys.append(j)
+    if len(keys) == len(boxes):
+        return True
+    return False
