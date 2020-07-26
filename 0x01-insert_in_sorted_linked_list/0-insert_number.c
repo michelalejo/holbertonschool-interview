5f49e76345e0e0b57e2aeffb89c1e76110a59717
michelalejo <michelmolina65@gmail.com>
2020-07-26 18:24:57 -0500

    0-insert_number.c

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -9,14 +9,15 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new = malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t));
 
-    if (head == NULL)
-        return (NULL);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = number;
+	new->next = *head;
+	*head = new;
 
-    new->n = number;
-    new->next = *head;
-    *head = new;
-
-    return (new);
+	return (*head);
 }
