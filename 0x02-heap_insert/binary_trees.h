04be233e410d4176490bf6aecb2b7f6436c51dfb
michelalejo <michelmolina65@gmail.com>
2020-08-03 17:29:36 -0500

    binary_trees.h

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -22,7 +22,12 @@ struct binary_tree_s
 typedef struct binary_tree_s heap_t;
 typedef struct binary_tree_s binary_tree_t;
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+heap_t *heap_insert(heap_t **root, int value);
+int heap_size(heap_t *root);
+int tree_is_perfect(const heap_t *tree);
+heap_t *heap_search(heap_t *root);
 
 #endif
