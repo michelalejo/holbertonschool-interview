c480d82acd4ff596ce30e7fb57b2dc5dca11f3d9
michelalejo <michelmolina65@gmail.com>
2020-08-11 22:36:41 -0500

    0-minoperations.py

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    """Minimum Operations"""
+    if n <= 1:
+        return 0
+
+    for i in range(n - 1, 0, -1):
+        if n % i == 0:
+            number = n // i
+            return number + minOperations(i)
