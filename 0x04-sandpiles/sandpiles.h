c90b08ce7b8344d99115e4344956fd294f7eaad0
michelalejo <michelmolina65@gmail.com>
2020-08-16 02:11:55 -0500

    sandpiles.h

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,6 +5,9 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sum_sandpiles(int grid1[3][3], int grid2[3][3]);
+void printgrid(int grid[3][3]);
+void balance_sandpile(int grid[3][3], int i, int j);
 
 
 #endif
