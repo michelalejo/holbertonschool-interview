8774c598ee467e6db6c297550cb9d84ff9802f75
Michelle Molina <michelmolina65@gmail.com>
2020-09-03 00:01:07 -0500

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that reads stdin line by line and computes metrics.
+Write a script that reads stdin line by line and computes metrics
 """
 
 import sys
@@ -8,25 +8,25 @@ import sys
 try:
     count = 0
     size = 0
-    errors = {"200": 0, "301": 0, "400": 0, "401": 0,
+    status_error = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
 
     for line in sys.stdin:
         count += 1
         data = line.split()
         num = data[-2]
-        if num in errors:
-            errors[num] += 1
+        if num in status_error:
+            status_error[num] += 1
         size += int(data[-1])
         if count % 10 == 0:
             print("File size: {}".format(size))
-            for i, j in sorted(errors.items()):
+            for i, j in sorted(status_error.items()):
                 if j > 0:
                     print("{}: {}".format(i, j))
 except KeyboardInterrupt:
     pass
 finally:
     print("File size: {}".format(size))
-    for i, j in sorted(errors.items()):
+    for i, j in sorted(status_error.items()):
         if j > 0:
             print("{}: {}".format(i, j))
