6f9bf44062200186f4e474723b275cd655bf4d71
michelalejo <michelmolina65@gmail.com>
2020-09-22 13:23:13 -0500

    0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,16 +8,16 @@ def validUTF8(data):
     count = 0
     for n in data:
         if count == 0:
-            if (n >> 3) == 0b11110:
-                count = 3
+            if (n >> 5) == 0b110:
+                count = 1
             elif (n >> 4) == 0b1110:
                 count = 2
-            elif (n >> 5) == 0b110:
-                count = 1
+            elif (n >> 3) == 0b11110:
+                count = 3
             elif (n >> 7):
                 return False
         else:
             if (n >> 6) != 0b10:
                 return False
             count -= 1
-    return count
+    return count == 0
