fe7b48aa039a554d341d78a66a86eaa0760ea30e
michelalejo <michelmolina65@gmail.com>
2020-10-08 02:44:58 -0500

    0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,28 +3,39 @@
 
 import sys
 
-try:
-    count = 0
-    size = 0
-    errors = {"200": 0, "301": 0, "400": 0, "401": 0,
-              "403": 0, "404": 0, "405": 0, "500": 0}
+dlist = {"size": 0,
+            "lines": 1}
 
-    for line in sys.stdin:
-        count += 1
-        data = line.split()
-        num = data[-2]
-        if num in errors:
-            errors[num] += 1
-        size += int(data[-1])
-        if count % 10 == 0:
-            print("File size: {}".format(size))
-            for i, j in sorted(errors.items()):
-                if j > 0:
-                    print("{}: {}".format(i, j))
-except KeyboardInterrupt:
-    pass
-finally:
-    print("File size: {}".format(size))
-    for key in sorted(errors.items()):
+errors = {"200": 0, "301": 0, "400": 0, "401": 0,
+            "403": 0, "404": 0, "405": 0, "500": 0}
+
+
+def printf():
+    """ Print codes and numbers"""
+    print("File size: {}".format(dlist["size"]))
+    for key in sorted(errors.keys()):
         if errors[key] != 0:
             print("{}: {}".format(key, errors[key]))
+
+
+def datasize(data):
+    """ Count file codes and size"""
+    dlist["size"] += int(data[-1])
+    if data[-2] in errors:
+        errors[data[-2]] += 1
+
+
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            try:
+                datasize(line.split(" "))
+            except:
+                pass
+            if dlist["lines"] % 10 == 0:
+                printf()
+            dlist["lines"] += 1
+    except KeyboardInterrupt:
+        printf()
+        raise
+    printf()
