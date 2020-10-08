2706a9151e71749730f7e198d6a7b305ec568eef
michelalejo <michelmolina65@gmail.com>
2020-10-08 01:59:34 -0500

    Update

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,12 +1,14 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
+#define SIZE 3
+#define IN_RANGE(x, y) (x >= 0 && x < 3 && y >= 0 && y < 3)
+
 #include <stdlib.h>
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void sum_sandpiles(int grid1[3][3], int grid2[3][3]);
+void balance_sandpile(int grid1[3][3], int i, int j);
 void printgrid(int grid[3][3]);
-void balance_sandpile(int grid[3][3], int i, int j);
 
 #endif
