9d501e3a90156ad00277e65c78e86de29be18b34
michelalejo <michelmolina65@gmail.com>
2020-10-09 14:33:44 -0500

    0-menger.c

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,34 @@
+#include "menger.h"
+
+/**
+ * menger - Function that draws a 2D Menger Sponge.
+ * @level: The level of the Menger Sponge to draw.
+ * Return: Void - No return.
+ */
+void menger(int level)
+{
+	int i = 0, j = 0, i_2 = 0, j_2 = 0;
+	int size = pow(3, level), space = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i_2 = i, j_2 = j, space = 0; j_2; i_2 /= 3, j_2 /= 3 || i_2)
+				if (i_2 % 3 == 1 && j_2 % 3 == 1)
+				{
+					space = 1;
+				}
+			if (space)
+			{
+				printf("%c", ' ');
+			}
+			else
+			{
+				printf("%c", '#');
+			}
+		}
+
+		printf("\n");
+	}
+}
