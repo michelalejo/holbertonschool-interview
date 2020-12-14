0c40678b18531418fe0918eeb66e1a0e27eab996
michelalejo <michelmolina65@gmail.com>
2020-12-14 18:41:39 -0500

    0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,11 +4,17 @@ const request = require('request');
 const ac = process.argv.slice(2);
 
 request.get(`https://swapi-api.hbtn.io/api/films/${ac}/`, async (_error, response, body) => {
-  const details = JSON.parse(body);
-  for (const characters of details.characters) {
-    request.get(characters, async (errors, response, body) => {
-      const character = JSON.parse(body);
-      console.log(character.name);
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    const charac = await new Promise((resolve, reject) => {
+      request(character, (errors, responses, bodys) => {
+        if (errors) {
+          reject(errors);
+        } else {
+          resolve(JSON.parse(bodys).name);
+        }
+      });
     });
+    console.log(charac);
   }
 });
