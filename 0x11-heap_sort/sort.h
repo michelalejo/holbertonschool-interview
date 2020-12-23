61d82a7133cd31ab2197488f150fd54c54e57e67
michelalejo <michelmolina65@gmail.com>
2020-12-23 18:12:18 -0500

    sort.h

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef heap_sort_h
+#define heap_sort_h
+
+#include <stdio.h>
+#include <stdlib.h>
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void swap_(int *a, int *b);
+void max_heap(int *arr, int index, int len, int size);
+
+#endif
