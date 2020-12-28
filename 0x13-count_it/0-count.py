4fab9ce5348aa6225580c205b0344162988742c1
michelalejo <michelmolina65@gmail.com>
2020-12-27 20:10:42 -0500

    0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -3,31 +3,36 @@
 """ Recursive function that queries the Reddit API"""
 
 import requests
+
+
 def count_words(subreddit, word_list, after='', words_counting={}):
     """ Recursive function that queries the Reddit API"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     h = {'User-Agent': 'python3:holberton.task:v1.0'}
     payload = {'limit': '100', 'after': after}
-    r = requests.get(url, headers=h, params=payload, allow_redirects=False)
-    if r.status_code == 200:
-        data = r.json().get('data')
+    response = requests.get(url, headers=h,
+                            params=payload, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get('data')
         after = data.get('after')
         children = data.get('children')
         for child in children:
             title = child.get('data').get('title')
             for word in word_list:
-                incident = title.lower().split().count(word.lower())
-                if incident > 0:
+                ocurrences = title.lower().split().count(word.lower())
+                if ocurrences > 0:
                     if word in words_counting:
-                        words_counting[word] += incident
+                        words_counting[word] += ocurrences
                     else:
-                        words_counting[word] = incident
+                        words_counting[word] = ocurrences
+
         if after is not None:
             return count_words(subreddit, word_list, after, words_counting)
         else:
             if not len(words_counting) > 0:
                 return
-            datas = sorted(words_counting.items(), key=lambda kv: (-kv[1], kv[0]))
+            datas = sorted(words_counting.items(),
+                            key=lambda key_value: (-key_value[1], key_value[0]))
             for key, value in datas:
                 print('{}: {}'.format(key.lower(), value))
     else:
